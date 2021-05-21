b3e4119d316bcee88ad22978d1da7a6881236e74
muratcanoncu <murat.oncu1992@hotmail.com>
2021-05-21 10:20:46 +0200

    multer set up

--- backend/routes/indexRoute.js
+++ backend/routes/indexRoute.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
-router.get("/show", indexController.showTodoList);
-router.post("/add", indexController.addTodoList);
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "public/uploads");
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
+router.post("/add", upload.single("itemPhoto"), indexController.addTodoList);
+
 // router.post("/delete", indexController);
 // router.post("/update", indexController);
-
+router.get("/show", indexController.showTodoList);
 module.exports = router;
